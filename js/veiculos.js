a3bb30216809eaff284f520aae02e12425d4bc95
josesilvaneto-create <jose.silvaneto@sou.unaerp.edu.br>
2025-11-26 11:51:38 -0300

    Update veiculos.js

--- js/veiculos.js
+++ js/veiculos.js
@@ -51,8 +51,7 @@ function exibirVeiculos(veiculos) {
         html += `
         <div class="vehicle-card">
             <div class="vehicle-image" style="background-image: url('${imagemUrl}'); background-size: cover; background-position: center;">
-                <div class="image-overlay">
-                    <span>${veiculo.marca} ${veiculo.modelo}</span>
+                
                     ${veiculo.imagens && veiculo.imagens.length > 1 ? 
                         `<small>+${veiculo.imagens.length - 1} foto(s)</small>` : ''}
                 </div>
@@ -405,3 +404,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
